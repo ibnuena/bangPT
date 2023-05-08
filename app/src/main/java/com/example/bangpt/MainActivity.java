a7cc55f96279caf44c90acb018425c96d2f59b73
Eunbi An <ibnuena@naver.com>
2023-05-09 01:01:05 +0900

    feat: home banner develop

--- app/src/main/java/com/example/bangpt/MainActivity.java
+++ app/src/main/java/com/example/bangpt/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.bangpt;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
 import android.os.Bundle;
 import android.view.MenuItem;
+
+import com.example.bangpt.banner.Banner1Fragment;
 import com.google.android.material.navigation.NavigationBarView;
+
+import me.relex.circleindicator.CircleIndicator3;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ViewPager2 mPager;
+    private FragmentStateAdapter pagerAdapter;
+    private int num_page = 3;
+    private CircleIndicator3 mIndicator;
     HomeFragment homeFragment;
     CalendarFragment calendarFragment;
     CommunityFragment communityFragment;
@@ -23,6 +34,37 @@ public class MainActivity extends AppCompatActivity {
         communityFragment = new CommunityFragment();
         mypageFragment = new MypageFragment();
         challengeFragment = new ChallengeFragment();
+
+
+//ViewPager2
+        mPager = findViewById(R.id.viewpager);
+        //Adapter
+        pagerAdapter = new MyAdapter(this, num_page);
+        mPager.setAdapter(pagerAdapter);
+        //Indicator
+        mIndicator = findViewById(R.id.indicator);
+        mIndicator.setViewPager(mPager);
+        mIndicator.createIndicators(num_page,0);
+        //ViewPager Setting
+        mPager.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
+        mPager.setCurrentItem(1002); //시작 지점
+        mPager.setOffscreenPageLimit(3); //최대 이미지 수
+
+        mPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+                                                @Override
+                                                public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                                                    super.onPageScrolled(position, positionOffset, positionOffsetPixels);
+                                                    if (positionOffsetPixels == 0) {
+                                                        mPager.setCurrentItem(position);
+                                                    }
+                                                }
+
+                                                @Override
+                                                public void onPageSelected(int position) {
+                                                    super.onPageSelected(position);
+                                                    mIndicator.animatePageSelected(position % num_page);
+                                                }
+                                            });
 
         getSupportFragmentManager().beginTransaction().replace(R.id.containers, homeFragment).commit();
 
---
+++ app/src/main/java/com/example/bangpt/banner/Banner2Fragment.java
@@ -0,0 +1,23 @@
+package com.example.bangpt.banner;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.bangpt.R;
+
+public class Banner2Fragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.fragment_banner2, container, false);
+
+        return rootView;
+    }
+}
