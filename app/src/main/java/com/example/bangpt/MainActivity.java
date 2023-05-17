19edfa6940320c9606dff5ce709f6d8c2f20aed0
김예지 <yejeejudy@naver.com>
2023-05-17 18:54:32 +0900

    디비연결

--- app/src/main/java/com/example/bangpt/MainActivity.java
+++ app/src/main/java/com/example/bangpt/MainActivity.java
@@ -7,9 +7,18 @@ import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.example.bangpt.banner.Banner1Fragment;
 import com.google.android.material.navigation.NavigationBarView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import me.relex.circleindicator.CircleIndicator3;
 
@@ -25,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
     MypageFragment mypageFragment;
     ChallengeFragment challengeFragment;
 
+    private TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +47,46 @@ public class MainActivity extends AppCompatActivity {
         challengeFragment = new ChallengeFragment();
 
 
+        textView = findViewById(R.id.text_view);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL("http://10.0.2.2:5000/");
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("GET");
+        /*
+                    try {
+                        int forTest = conn.getResponseCode(); // 응답 상태 코드 저장
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+        */
+
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+                    String line;
+                    StringBuilder sb = new StringBuilder();
+
+                    while ((line = reader.readLine()) != null) {
+                        sb.append(line);
+                    }
+
+                    final String response = sb.toString();
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            textView.setText(response);
+                        }
+                    });
+
+                }catch(Exception e){
+                //    textView.setText("error");
+                    e.printStackTrace();
+                }
+            }
+        }).start();
 //ViewPager2
         mPager = findViewById(R.id.viewpager);
         //Adapter
