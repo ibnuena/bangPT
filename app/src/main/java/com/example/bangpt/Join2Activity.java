01d4a0d11ed291f3aa96093b1838cbe623a6a884
hyeyeon <gpsdl2130@gmail.com>
2023-06-03 21:10:38 +0900

    trainer photo update

--- app/src/main/java/com/example/bangpt/Join2Activity.java
+++ app/src/main/java/com/example/bangpt/Join2Activity.java
@@ -1,38 +1,143 @@
 package com.example.bangpt;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.content.CursorLoader;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.bangpt.Request.VolleyMultipartRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 
 public class Join2Activity extends AppCompatActivity {
-    private Button btn_register;
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private Button btnUpload;
+    private ImageView imageView;
+    private Bitmap bitmap;
+    private String URL = "http://10.0.2.2:821/trainers/get_photo"; // 서버 엔드포인트 URL로 변경해주세요
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join2);
 
-        btn_register=findViewById(R.id.btn_register);
-        btn_register.setOnClickListener(new View.OnClickListener(){
-            @Override
-            //회원 가입 후 메인화면으로
-            public void onClick(View view){
-                Intent intent=new Intent(Join2Activity.this,MainActivity.class);
-                startActivity(intent);
-            }
-        });
-        Button galleryButton = findViewById(R.id.btn_upload);
-        galleryButton.setOnClickListener(new View.OnClickListener() {
+        btnUpload = findViewById(R.id.btn_upload);
+
+        btnUpload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openGallery();
             }
         });
     }
+
     private void openGallery() {
-        Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        startActivity(intent);
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Image"), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null && data.getData() != null) {
+            try {
+                // 선택한 이미지 가져오기
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), data.getData());
+
+                // 서버에 이미지 업로드
+                uploadImage();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void uploadImage() {
+        // Volley를 사용하여 이미지를 서버로 전송
+        VolleyMultipartRequest volleyMultipartRequest = new VolleyMultipartRequest(Request.Method.POST, URL,
+                new Response.Listener<NetworkResponse>() {
+                    @Override
+                    public void onResponse(NetworkResponse response) {
+                        try {
+                            JSONObject obj = new JSONObject(new String(response.data));
+                            String message = obj.getString("message");
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(Join2Activity.this, message, Toast.LENGTH_SHORT).show();
+                                }
+                            });
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Log.e("JSONException", e.getMessage());
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), "Error: Image upload failed.", Toast.LENGTH_SHORT).show();
+                    }
+
+                }) {
+            @Override
+            protected Map<String, VolleyMultipartRequest.DataPart> getByteData() {
+                Map<String, DataPart> params = new HashMap<>();
+                String imageData = imageToString(bitmap);
+                params.put("image", new DataPart("image.jpg", Base64.decode(imageData, Base64.DEFAULT)));
+                return params;
+            }
+        };
+
+        Volley.newRequestQueue(Join2Activity.this).add(volleyMultipartRequest);
+    }
+
+
+    private String imageToString(Bitmap bitmap) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+        byte[] imageBytes = outputStream.toByteArray();
+        return Base64.encodeToString(imageBytes, Base64.DEFAULT);
     }
 }
+
+
